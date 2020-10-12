9ccec5fdaf9d9dfbcd3b5fe76f38ee810b00af5f
meliksah cakir <mlkshckr@gmail.com>
2020-10-13 00:52:34 +0300

    Initialize project

---
+++ gradlew.bat
@@ -0,0 +1,35 @@
+{
+    "logs": [
+        {
+            "outputFile": "C:\\Users\\Meliksah\\AndroidStudioProjects\\TemplateProjectName\\app\\build\\intermediates\\incremental\\mergeDebugResources\\merged.dir\\values-lo\\values-lo.xml",
+            "map": [
+                {
+                    "source": "C:\\Users\\Meliksah\\.gradle\\caches\\transforms-2\\files-2.1\\6fdb546414c8ca416b15b3d245bdba2c\\core-1.3.0\\res\\values-lo\\values-lo.xml",
+                    "from": {
+                        "startLines": "2",
+                        "startColumns": "4",
+                        "startOffsets": "55",
+                        "endColumns": "100",
+                        "endOffsets": "151"
+                    },
+                    "to": {
+                        "startLines": "32",
+                        "startColumns": "4",
+                        "startOffsets": "3072",
+                        "endColumns": "100",
+                        "endOffsets": "3168"
+                    }
+                },
+                {
+                    "source": "C:\\Users\\Meliksah\\.gradle\\caches\\transforms-2\\files-2.1\\ad2e89686814680d27b571e99acbb326\\appcompat-1.1.0\\res\\values-lo\\values-lo.xml",
+                    "from": {
+                        "startLines": "2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29",
+                        "startColumns": "4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4",
+                        "startOffsets": "105,208,311,424,509,613,724,802,880,971,1063,1155,1249,1350,1443,1538,1634,1725,1816,1896,2003,2107,2205,2308,2412,2516,2673,2772",
+                        "endColumns": "102,102,112,84,103,110,77,77,90,91,91,93,100,92,94,95,90,90,79,106,103,97,102,103,103,156,98,80",
+                        "endOffsets": "203,306,419,504,608,719,797,875,966,1058,1150,1244,1345,1438,1533,1629,1720,1811,1891,1998,2102,2200,2303,2407,2511,2668,2767,2848"
+                    },
+                    "to": {
+                        "startLines": "2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,31",
+                        "startColumns": "4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4",
+                        "startOffsets": "105,208,311,424,509,613,724,8
